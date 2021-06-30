139782747afba044acc14dd3a88d6e92047429ce
ShivaVkm <shivavkm2001@gmail.com>
2021-06-30 14:38:15 +0530

    swappingOfNumbers.py gets added

---
+++ swappingOfNumbers.py
@@ -0,0 +1,13 @@
+# Swapping of numbers in different ways
+
+num1 = 10
+num2 = 5
+print("Numbers before swapping:")
+print("num1 = %d"%num1, end = " ")
+print("num2 = %d"%num2)
+
+temp = num1
+num1 = num2 
+num2 = temp
+print("num1 = %d"%num1, end = " ")
+print("num2 = %d"%num2)

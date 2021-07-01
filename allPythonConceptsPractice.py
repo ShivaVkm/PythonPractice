4a96c408de88e9ad68857e4eceb8e5726b5b0427
ShivaVkm <shivavkm2001@gmail.com>
2021-07-01 12:36:16 +0530

    allPythonConceptsPractice.py file gets committed here

---
+++ allPythonConceptsPractice.py
@@ -0,0 +1,175 @@
+# pythonLists = []
+# pythonLists.append(12)
+# print(pythonLists)
+
+# pythonLists.append("This is List's Application.")
+
+# print(pythonLists)
+
+
+
+
+
+
+
+
+
+# name = str(input("Enter your name:"))
+# print("hello",name )
+
+
+
+
+
+
+
+
+
+
+# def add(a,b):
+#     return a+b
+
+
+# if __name__ == "__main__":
+#     print(add(34,34))
+
+
+
+
+
+
+
+# Python range() Function
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+
+# for i in range(10):
+#     print(i)
+
+
+
+
+
+
+
+
+# import math
+# num = -23
+# print(num)
+# result = math.fabs(num)
+# print(result)
+
+
+
+
+
+
+
+
+
+
+# num = -23
+# print(num)
+# result = abs(num)
+# print(result)
+
+
+
+
+
+
+
+
+# import keyword
+# listsOfKeywords = ["if", "else", "rohan"]
+# for i in range(len(listsOfKeywords)):
+#     if(keyword.iskeyword(listsOfKeywords[i])):
+#         print(listsOfKeywords[i]," is a python keyword.")
+#     else:
+#         print("its not")
+
+
+
+
+
+
+
+
+
+# # importing "keyword" for keyword operations
+# import keyword
+
+# # printing all keywords at once using "kwlist()"
+# print ("The list of keywords is : ")
+# print (keyword.kwlist())
+
+
+
+
+
+
+
+# # input
+# num1 = float(input())
+# num2 = float(input())
+  
+# # printing the sum in float
+# print(num1 + num2)
+
+
+
+
+
+
+
+
+
+
+# # Python program showing how to
+# # multiple input using split
+
+# # taking two inputs at a time
+# x, y = input("Enter a two value: ").split()
+# print("Number of boys: ", x)
+# print("Number of girls: ", y)
+# print()
+
+# # taking three inputs at a time
+# x, y, z = input("Enter a three value: ").split()
+# print("Total number of students: ", x)
+# print("Number of boys is : ", y)
+# print("Number of girls is : ", z)
+# print()
+
+# # taking two inputs at a time
+# a, b = input("Enter a two value: ").split()
+# print("First number is {} and second number is {}".format(a, b))
+# print()
+
+# # taking multiple inputs at a time
+# # and type casting using list() function
+# x = list(map(int, input("Enter a multiple value: ").split()))
+# print("List of students: ", x)
+
+
+
+
+
+
+
+# print("geeks",end = " ")
+# print("geeksforgeeks")
+
+
+
+
+
+
+
+# print('Programmer')
+# print('20','01','2018', sep='-')
+# print('rohan','hackerrank', sep='@')
+
+
+
+
+

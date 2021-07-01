23ff0ecf3fb02d9dd189fdaa526d4650b273dbba
ShivaVkm <shivavkm2001@gmail.com>
2021-07-01 21:05:41 +0530

    inbuiltPolymorphicFunctions.py file gets committed here

---
+++ inbuiltPolymorphicFunctions.py
@@ -0,0 +1,4 @@
+# same function name but having different parameters are called polymorhic functions / polymophism
+function1 = len("Coders")
+function2 = len([10, 20, 30])
+print(function1,"   ",function2)

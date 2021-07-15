5958cd2090cb0d3a58801818d0b9471e27070a1c
ShivaVkm <shivavkm2001@gmail.com>
2021-07-15 22:45:23 +0530

    listInputOutput.py file gets committed here

---
+++ listInputOutput.py
@@ -0,0 +1,7 @@
+listOfNumbers = []
+n = int(input("Enter the number to make its list:"))
+for i in range(0, n):
+    numbers = int(input())
+    listOfNumbers.append(numbers)
+
+print(listOfNumbers)

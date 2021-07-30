dfc3883a3b989b61b2b64e7d17e1d1e6f5397d20
ShivaVkm <shivavkm2001@gmail.com>
2021-07-30 19:01:52 +0530

    playWithTrueFalse.py file gets committed here

---
+++ playWithTrueFalse.py
@@ -0,0 +1,24 @@
+# True means 1 and False in 0
+print(True) # you will get True
+print(False) # you will get False
+print(True+False) # you will get 1 because True = 1 and False = 0
+print(True == 1) # verification for trueness' value as equal to 1
+print(False == 0) # verification for falseness' value as equal to 0
+
+print(True+True)
+print(True-True)
+print(True*True)
+print(True/True)
+print(True%True)
+
+print(False+False)
+print(False-False)
+print(False*False)
+# print(False/False)  # ZeroDivisionError: division by zero
+# print(False%False)    #  ZeroDivisionError: integer division or modulo by zero
+
+print(True+False)
+print(True-False)
+print(True*False)
+# print(True/False) # ZeroDivisionError: division by zero
+# print(True%False) # ZeroDivisionError: integer division or modulo by zero
